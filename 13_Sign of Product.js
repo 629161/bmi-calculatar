7a306fb9b5083ce7fb4840383bbdb6afd6a4515e
sumanrkms58@gmail.com <sumanrkms58@gmail.com>
2023-11-29 16:09:51 +0530

    Sign of Product

---
+++ 13_Sign of Product.js
@@ -0,0 +1,18 @@
+function signOfProduct(nums) {
+    let negativeCount = 0;
+  
+    for (let num of nums) {
+      if (num === 0) {
+        return 0;
+      } else if (num < 0) {
+        negativeCount++;
+      }
+    }
+  
+    if (negativeCount % 2 === 0) {
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+  
